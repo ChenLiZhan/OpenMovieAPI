da5f271a2acf5e82b3b44cfc9bc07ef3d2e05f50
davidyang <pingjing.yang@gmail.com>
2014-12-12 21:27:13 +0800

    add helpers

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'yaml'
 require_relative 'model/movie'
 require_relative 'model/theater'
 
-class MovieApp < Sinatra::Base
+class MovieAppDynamo < Sinatra::Base
   set :views, Proc.new { File.join(root, "views") }
   # enable :sessions
   use Rack::Session::Pool
@@ -15,11 +15,10 @@ class MovieApp < Sinatra::Base
     enable :logging
   end
 
-  API_BASE_URI = 'http://localhost:4567'
-
   helpers do
     # RANK_LIST = { '1' => 'U.S.', '2' => 'Taiwan', '3' => 'DVD' }
 
+    # return one movie info
     def get_movie_info(moviename)
       # begin
         # halt 404 if moviename == nil?
@@ -31,6 +30,7 @@ class MovieApp < Sinatra::Base
         movie_crawled
     end
 
+    # return a theater info
     def get_ranks(category)
       halt 404 if category.to_i > 3
       ranks_after = {
@@ -43,6 +43,7 @@ class MovieApp < Sinatra::Base
       ranks_after
     end
 
+    # return a theater info
     def get_infos(category)
       halt 404 if category == nil?
       infos_after = {
@@ -55,6 +56,7 @@ class MovieApp < Sinatra::Base
       infos_after
     end
 
+    # get a multiple info.
     def topsum(n)
       us1 = YAML.load(MovieCrawler::us_weekend).reduce(&:merge)
       tp1 = YAML.load(MovieCrawler::taipei_weekend).reduce(&:merge)
@@ -66,9 +68,25 @@ class MovieApp < Sinatra::Base
         { k => [{us:us1[k] || "0" }, { tp:tp1[k] || "0" }, { dvd:dvd1[k] || "0"}] }
       end
     end
+
+    def new_movie(req)
+      movie = Movie.new
+      movie.moviename = req['moviename'].to_json
+      movie.movieinfo = get_movie_info(req['movieinfo']).to_json
+      movie
+    end
+
+    def new_theater(req)
+      theater = Theater.new
+      theater.content_type = req['content_type'].to_json
+      theater.category = req['category'].to_json
+      theater.content = req['content'].to_json
+      theater
+    end
+
   end
 
-  after { ActiveRecord::Base.connection.close }
+  # after { ActiveRecord::Base.connection.close }
 
   get '/' do
     'The API are working.'
@@ -88,9 +106,11 @@ class MovieApp < Sinatra::Base
 
     movie = Movie.find_by(moviename: req['movie'])
     if movie.nil?
-      movie = Movie.new
-      movie.moviename = req['movie']
-      movie.movieinfo = get_movie_info(req['movie']).to_json
+
+      movie = new_movie(req)
+      # movie = Movie.new
+      # movie.moviename = req['movie']
+      # movie.movieinfo = get_movie_info(req['movie']).to_json
       movie.save
     end
 
@@ -122,10 +142,13 @@ class MovieApp < Sinatra::Base
     else
       data = params[:type] == 'info' ? get_infos(params[:category]) : \
       get_ranks(params[:category])
-      theater = Theater.new
-      theater.content_type = data['content_type']
-      theater.category = data['category']
-      theater.content = data['content'].to_json
+
+
+      theater = new_theater(data)
+      # theater = Theater.new
+      # theater.content_type = data['content_type']
+      # theater.category = data['category']
+      # theater.content = data['content'].to_json
       theater.save && data.to_json
     end
   end
